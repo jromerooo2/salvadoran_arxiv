ba4f4a02bac34912cae63383a7c85d3d852b0253
jromerooo2 <juan.romeroramos9@gmail.com>
2024-09-19 10:45:08 -0500

    commit

--- src/views/Contacto.vue
+++ src/views/Contacto.vue
@@ -1,13 +1,6 @@
 <template>
     <div>
       <p></p>
-      <!--
-  Heads up! 👋
-
-  Plugins:
-    - @tailwindcss/forms
--->
-
   <section class="relative flex flex-wrap lg:h-screen lg:items-center">
     <div class="w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-8 lg:py-24">
       <div class="mx-auto max-w-lg text-center">
