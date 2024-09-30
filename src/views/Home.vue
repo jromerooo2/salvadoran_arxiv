f364a13695058b2d110cef6052e399966aefd941
erickurquilla1999 <erickurquilla1999@gmail.com>
2024-09-30 13:55:27 -0400

    Creating structure in home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,52 +5,41 @@
     <h3>El Repositorio Salvadoreño de Ciencia (RSC) es una plataforma dedicada a la difusion gratuita de artículos científicos realizados por investigadores salvadoreños. Este repositorio permite que la ciencia producida en El Salvador sea visible y accesible tanto a nivel local como internacional. Los temas abarcan desde las ciencias naturales, ingeniería, ciencias sociales y otros campos del conocimiento, y buscan promover la colaboración y difusión de la ciencia hecha en El Salvador.</h3>
   </div>
 
-  <!-- SEARCH BAR -->
+  <!-- Search bar -->
   <div class="search-bar">
-
     <input
       type="text"
       class="search-input"
       placeholder="Search..."
       v-model="searchQuery"
     />
-
     <select v-model="selectedField" class="search-select">
       <option>All fields</option>
       <option>Title</option>
       <option>Author</option>
       <option>Abstract</option>
     </select>
-    
     <button @click="performSearch" class="search-button">Search</button>
-
   </div>
   
   <!-- Table of content -->
   <div class="category-index">
     <ul>
       <li v-for="(category, index) in table_of_content" :key="index">
-        
         <h1>{{ category.category_name }}
           <span class="links">
             (<a :href="category.category_recentLink">recent</a>, 
             <a :href="category.category_searchLink">search</a>)
           </span>
         </h1>
-        
-        <span v-if="category.subcategories.length">
-          <p>
-            <span v-for="(subcategory, subIndex) in category.subcategories" :key="subIndex">
-              <a>{{ subcategory.name }}
-                <span class="links">
-                  (<a :href="subcategory.recentLink">recent</a>, 
-                  <a :href="subcategory.searchLink">search</a>)
-                </span>
-              </a>
-              <span v-if="subIndex !== category.subcategories.length - 1">, </span>
+        <div v-for="(subcategory, subIndex) in category.subcategories" :key="subIndex">
+          <b>{{ subcategory.subcategories_name }}
+            <span class="links">
+              (<a :href="subcategory.subcategories_recentLink">recent</a>, 
+              <a :href="subcategory.subcategories_searchLink">search</a>)
             </span>
-          </p>
-        </span> 
+          </b>
+        </div>
       </li>
     </ul>
   </div>
@@ -70,212 +59,98 @@
 
 
 <script>
-export default {
-  data() {
-    return {
-      searchQuery: '',
-      selectedField: 'All fields',
+  export default {
 
+    data() {
+      return {
+        searchQuery: '',
+        selectedField: 'All fields',
 
-      table_of_content: [
-        {
+        table_of_content: [
+          {
 
-          category_name : "Fisica",
-          category_recentLink: "#",
-          category_searchLink: "#",
+            category_name : "Fisica",
+            category_recentLink: "#",
+            category_searchLink: "#",
 
-          subcategories: [
-            {
-              name: "Astrophysics (astro-ph)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: "Astrophysics of Galaxies; Cosmology and Nongalactic Astrophysics; Earth and Planetary Astrophysics"
-            },
-            {
-              name: "Condensed Matter (cond-mat)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: "Disordered Systems and Neural Networks; Materials Science; Superconductivity"
-            },
-            {
-              name: "General Relativity and Quantum Cosmology (gr-qc)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: ""
-            },
-            {
-              name: "High Energy Physics - Experiment (hep-ex)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: ""
-            },
-            {
-              name: "High Energy Physics - Lattice (hep-lat)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: ""
-            },
-            {
-              name: "High Energy Physics - Phenomenology (hep-ph)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: ""
-            },
-            {
-              name: "High Energy Physics - Theory (hep-th)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: ""
-            },
-            {
-              name: "Mathematical Physics (math-ph)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: ""
-            },
-            {
-              name: "Nonlinear Sciences (nlin)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: "Adaptation and Self-Organizing Systems; Chaotic Dynamics; Exactly Solvable and Integrable Systems"
-            },
-            {
-              name: "Nuclear Theory (nucl-th)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: ""
-            },
-            {
-              name: "Physics (physics)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: "Accelerator Physics; Applied Physics; Atomic and Molecular Clusters; Instrumentation and Detectors"
-            },
-            {
-              name: "Quantum Physics (quant-ph)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: ""
-            }
-          ],
-        },
-        {
+            subcategories: [
+              {
+                subcategories_name: "Astrophysics (astro-ph)",
+                subcategories_recentLink: "#",
+                subcategories_searchLink: "#",
+              },
+              {
+                subcategories_name: "Condensed Matter (cond-mat)",
+                subcategories_recentLink: "#",
+                subcategories_searchLink: "#",
+              },
+              {
+                subcategories_name: "General Relativity and Quantum Cosmology (gr-qc)",
+                subcategories_recentLink: "#",
+                subcategories_searchLink: "#",
+              }
+            ],
+          },
+          {
 
-          category_name : "Quimica",
-          category_recentLink: "#",
-          category_searchLink: "#",
-          
-          subcategories: [
-            {
-              name: "Astrophysics (astro-ph)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: "Astrophysics of Galaxies; Cosmology and Nongalactic Astrophysics; Earth and Planetary Astrophysics"
-            },
-            {
-              name: "Condensed Matter (cond-mat)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: "Disordered Systems and Neural Networks; Materials Science; Superconductivity"
-            },
-            {
-              name: "General Relativity and Quantum Cosmology (gr-qc)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: ""
-            },
-            {
-              name: "High Energy Physics - Experiment (hep-ex)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: ""
-            },
-            {
-              name: "High Energy Physics - Lattice (hep-lat)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: ""
-            },
-            {
-              name: "High Energy Physics - Phenomenology (hep-ph)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: ""
-            },
-            {
-              name: "High Energy Physics - Theory (hep-th)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: ""
-            },
-            {
-              name: "Mathematical Physics (math-ph)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: ""
-            },
-            {
-              name: "Nonlinear Sciences (nlin)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: "Adaptation and Self-Organizing Systems; Chaotic Dynamics; Exactly Solvable and Integrable Systems"
-            },
-            {
-              name: "Nuclear Theory (nucl-th)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: ""
-            },
-            {
-              name: "Physics (physics)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: "Accelerator Physics; Applied Physics; Atomic and Molecular Clusters; Instrumentation and Detectors"
-            },
-            {
-              name: "Quantum Physics (quant-ph)",
-              newLink: "#",
-              recentLink: "#",
-              searchLink: "#",
-              subcategories: ""
-            }
-          ],
-        },
-      ],
-    };
-  },
+            category_name : "Fisica",
+            category_recentLink: "#",
+            category_searchLink: "#",
 
-  methods: {
-    performSearch() {
-      // This is where the search logic would go
-      console.log(`Searching for "${this.searchQuery}" in ${this.selectedField}`);
+            subcategories: [
+              {
+                subcategories_name: "Astrophysics (astro-ph)",
+                subcategories_recentLink: "#",
+                subcategories_searchLink: "#",
+              },
+              {
+                subcategories_name: "Condensed Matter (cond-mat)",
+                subcategories_recentLink: "#",
+                subcategories_searchLink: "#",
+              },
+              {
+                subcategories_name: "General Relativity and Quantum Cosmology (gr-qc)",
+                subcategories_recentLink: "#",
+                subcategories_searchLink: "#",
+              }
+            ],
+          },
+          {
+
+            category_name : "Fisica",
+            category_recentLink: "#",
+            category_searchLink: "#",
+
+            subcategories: [
+              {
+                subcategories_name: "Astrophysics (astro-ph)",
+                subcategories_recentLink: "#",
+                subcategories_searchLink: "#",
+              },
+              {
+                subcategories_name: "Condensed Matter (cond-mat)",
+                subcategories_recentLink: "#",
+                subcategories_searchLink: "#",
+              },
+              {
+                subcategories_name: "General Relativity and Quantum Cosmology (gr-qc)",
+                subcategories_recentLink: "#",
+                subcategories_searchLink: "#",
+              }
+            ],
+          }
+        ],
+      };
+    },
+
+    methods: {
+      performSearch() {
+        // This is where the search logic would go
+        console.log(`Searching for "${this.searchQuery}" in ${this.selectedField}`);
+      }
     }
-  }
 
-};
+  };
 </script>
 
 
@@ -290,124 +165,118 @@ export default {
 
 
 <style scoped>
-.container {
-  /* max-width: 900px; */
-  /* margin: 0 auto; */
-  font-family: Arial, sans-serif;
-}
+  
+  .container {
+    /* max-width: 900px; */
+    /* margin: 0 auto; */
+    font-family: Arial, sans-serif;
+  }
 
+  h3 {
+    font-family: Arial;
+    font-size: 15px; /* Increase the size */
+    text-align: left; /* Center it on the page */
+    color: #06080e; /* Change the color */
+    margin-top: 10px; /* Add some space below the title */
+    margin-bottom: 10px; /* Add some space below the title */
+    letter-spacing: 0px; /* Add some spacing between letters */
+  }
 
+  .search-bar {
+    display: flex;
+    align-items: center;
+    background-color: #ffffff; /* Red background color */
+    padding: 5px;
+  }
 
+  .search-input {
+    padding: 8px;
+    width: 200px;
+    border: 1px solid #ddd;
+    border-right: none;
+    border-radius: 4px 0 0 4px;
+  }
 
+  .search-select {
+    padding: 8px;
+    border: 1px solid #ddd;
+    border-left: none;
+    border-right: none;
+    border-radius: 0;
+    background-color: white;
+  }
 
-h3 {
-  font-family: Arial;
-  font-size: 15px; /* Increase the size */
-  text-align: left; /* Center it on the page */
-  color: #06080e; /* Change the color */
-  margin-top: 10px; /* Add some space below the title */
-  margin-bottom: 10px; /* Add some space below the title */
-  letter-spacing: 0px; /* Add some spacing between letters */
-}
+  .search-button {
+    padding: 8px 16px;
+    background-color: #0025a0;
+    color: white;
+    border: 1px solid #0025a0;
+    border-radius: 0 4px 4px 0;
+    cursor: pointer;
+  }
 
-.search-bar {
-  display: flex;
-  align-items: center;
-  background-color: #ffffff; /* Red background color */
-  padding: 5px;
-}
+  .search-button:hover {
+    background-color: #0025a0;
+  }
 
-.search-input {
-  padding: 8px;
-  width: 200px;
-  border: 1px solid #ddd;
-  border-right: none;
-  border-radius: 4px 0 0 4px;
-}
+  .category-index {
+    font-family: Arial, sans-serif;
+    font-size: 14px;
+  }
 
-.search-select {
-  padding: 8px;
-  border: 1px solid #ddd;
-  border-left: none;
-  border-right: none;
-  border-radius: 0;
-  background-color: white;
-}
+  .category-index ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-.search-button {
-  padding: 8px 16px;
-  background-color: #0025a0;
-  color: white;
-  border: 1px solid #0025a0;
-  border-radius: 0 4px 4px 0;
-  cursor: pointer;
-}
+  .category-index li {
+    margin-bottom: 5px;
+  }
 
-.search-button:hover {
-  background-color: #0025a0;
-}
+  .category-index a {
+    color: #4b0082;
+    text-decoration: none;
+  }
 
-.category-index {
-  font-family: Arial, sans-serif;
-  font-size: 14px;
-}
+  .category-index a:hover {
+    text-decoration: underline;
+  }
 
-.category-index ul {
-  list-style-type: none;
-  padding: 0;
-}
+  b {
+    color: #000000;
+    text-decoration: none;
+    font-size: 12px;
+  }
 
-.category-index li {
-  margin-bottom: 5px;
-}
+  .links {
+    font-size: 12px;
+  }
 
-.category-index a {
-  color: #4b0082;
-  text-decoration: none;
-}
+  .subcategories {
+    font-size: 12px;
+    color: #555;
+    margin-left: 10px;
+  }
 
-.category-index a:hover {
-  text-decoration: underline;
-}
+  /* Table of content style */
 
-.links {
-  font-size: 12px;
-}
+  h1 {
+    font-size: 24px;
+    color: #333;
+  }
 
-.subcategories {
-  font-size: 12px;
-  color: #555;
-  margin-left: 10px;
-}
+  h2 {
+    font-size: 20px;
+    color: #555;
+  }
 
-/* Table of content style */
+  ul {
+    list-style-type: none;
+    padding-left: 0;
+  }
 
-h1 {
-  font-size: 24px;
-  color: #333;
-}
-
-h2 {
-  font-size: 20px;
-  color: #555;
-}
-
-ul {
-  list-style-type: none;
-  padding-left: 0;
-}
-
-li {
-  margin: 5px 0;
-}
-
-a {
-  color: #007bff;
-  text-decoration: none;
-}
-
-a:hover {
-  text-decoration: underline;
-}
+  li {
+    margin: 5px 0;
+  }
 
 </style>
