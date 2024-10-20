9f85ec485543fb06f1f0fe8b09599d93cb6c80c0
erickurquilla1999 <erickurquilla1999@gmail.com>
2024-10-20 16:21:31 -0400

    Cleaning up App.vue file

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,12 @@
     <router-view/>
   </div>
 </template>
-<script>
-import Navbar from './components/Navbar.vue';
 
-export default {
-  components: {
-    Navbar,
-  },
-};
+<script>
+  import Navbar from './components/Navbar.vue';
+  export default {
+    components: {
+      Navbar,
+    },
+  };
 </script>
