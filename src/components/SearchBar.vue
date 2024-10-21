758fd04ceae626e4ca93ce95990b1ece008a7d68
erickurquilla1999 <erickurquilla1999@gmail.com>
2024-10-20 21:46:26 -0400

    Update search bar style

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,17 +1,36 @@
 <template>
-    <div>
-        <input type="text" v-model="searchQuery" placeholder="Search..." />
-        <select v-model="searchField">
-            <option value="all">All fields</option>
-            <option value="title">Title</option>
-            <option value="description">Description</option>
-            <option value="author">Author</option>
-            <option value="date_published">Date Published</option>
-            <option value="subject">Subject</option>
-        </select>
-        <button @click="performSearch">Search</button>
+    <div class="my-4">
+      <!-- Smaller search input with blue background -->
+      <input 
+        type="text" 
+        v-model="searchQuery" 
+        placeholder="Search..." 
+        class="w-full md:w-2/5 px-3 py-1 bg-white border border-black rounded-md text-black text-sm focus:bg-gray-100 focus:outline-none focus:border-gray-600 focus:ring-2 focus:ring-gray-600 mb-2"
+      />
+  
+      <!-- Smaller select dropdown with blue background -->
+      <select 
+        v-model="searchField" 
+        class="w-full md:w-1/5 px-3 py-1 bg-white border border-black rounded-md text-black text-sm focus:bg-gray-100 focus:outline-none focus:border-gray-600 focus:ring-2 focus:ring-gray-600 mb-2"
+      >
+        <option value="all">All fields</option>
+        <option value="title">Title</option>
+        <option value="description">Description</option>
+        <option value="author">Author</option>
+        <option value="date_published">Date Published</option>
+        <option value="subject">Subject</option>
+      </select>
+  
+      <!-- Smaller search button -->
+      <button 
+        @click="performSearch" 
+        class="w-full md:w-auto bg-blue-600 hover:bg-blue-700 text-white font-semibold py-1 px-4 rounded-md text-sm transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+      >
+        Search
+      </button>
     </div>
-</template>
+  </template>
+  
   
 <script>
 
