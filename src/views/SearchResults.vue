a6c2fd6df019e465982965054edd0f20c31da726
erickurquilla1999 <erickurquilla1999@gmail.com>
2024-10-20 21:19:17 -0400

    Incluyendo la barra de busqueda en los resultados de busqueda

--- src/views/SearchResults.vue
+++ src/views/SearchResults.vue
@@ -1,4 +1,8 @@
 <template>
+    
+    <!-- Search bar -->
+    <Search_bar />
+
     <div class="md:grid md:grid-cols-3 md:gap-8 flex flex-col">
         <div v-for="article in results" :key="article.id" class="">
             <Tarjeta :id="article.id" :title="article.title" :subject="article.subject" :description="article.description" :author="article.author" :date_published="article.date_published" />
@@ -7,12 +11,14 @@
 </template>
   
 <script>
-
+    // Import the SearchBar component
+    import Search_bar from '../components/SearchBar.vue';
     import Tarjeta from '../components/Tarjeta.vue';
     
     export default {
         components: {
-            Tarjeta
+            Tarjeta,
+            Search_bar
         },
         data() {
             return {
