3565ff2698507ecad319b069f55912830253533f
erickurquilla <erickurq@gmail.com>
2026-05-10 10:32:52 -0400

    when i new page is open scroll up automatically

--- src/router.js
+++ src/router.js
@@ -19,6 +19,15 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    if (to.hash) {
+      return { el: to.hash, top: 0, behavior: 'smooth' }
+    }
+    return { top: 0, left: 0 }
+  },
 });
 
 export default router;
